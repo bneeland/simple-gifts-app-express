b68be393f9d680f22bbfb6a52a1035f7341e478c
Brian Neeland <brian.neeland@protonmail.com>
2022-01-17 10:31:35 -0700

    Post endpoint defined and working, tested with curl

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+})
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
